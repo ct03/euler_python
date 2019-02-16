6c9850b6a0f31a013f802178235d99f662d27330
ct03 <cptra.n@gmail.com>
2019-02-16 15:59:01 -0600

    Add files via upload

---
+++ euler2.py
@@ -0,0 +1,23 @@
+#euler problem 2
+
+        
+#def f():
+x,y,z,sum,i = 1,2,0,0,0
+
+while sum < 4000000:
+    if i == 0:
+        z=1
+    elif i ==1:
+        z=2
+    else:
+        z = x+y
+        x=y
+        y=z
+    if z%2 == 0:
+        sum+=z
+    i+=1
+    
+print(sum)
+        
+#f()
+        
